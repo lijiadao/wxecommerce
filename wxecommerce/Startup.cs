62976e5211305baebbb7c750258a7908f5772ee2
lijiadao <550920872@qq.com>
2017-01-09 22:09:19 +0800

    添加项目文件。

---
+++ wxecommerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wxecommerce.Startup))]
+namespace wxecommerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
